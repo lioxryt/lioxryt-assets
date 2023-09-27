1c5ea9278170975dbe88e136d9203d1cc8efc276
lee·ox·er·it <91391994+lioxryt@users.noreply.github.com>
2023-09-27 17:44:19 -0400

    llllll

--- other/roblox/script.js
+++ other/roblox/script.js
@@ -12,7 +12,7 @@ const video = document.getElementById('background-video');
                 const ipInfoItemContainers = document.querySelectorAll('.container .ip-info-item');
                 const ipInfoElements = document.querySelectorAll('.container span.hidden');
                 const h1Element = document.querySelector('.container h1'); // Get the <h1> element
-                const delays = [8500, 9000, 9500, 10000, 10500, 11000, 11500, 12000, 12500, 13000, 13500, 14000, 14500, 15000, 15500, 16000, 16500];                
+                const delays = [8500, 9000, 9500, 10000, 10500, 11000, 11500, 12000, 12500, 13000, 13500, 14000, 14500, 15000, 15500, 16000, 16500,18000,18500,19000];                
                 ipInfoElements.forEach((element, index) => {
                     setTimeout(() => {
                         element.classList.remove('hidden');
@@ -20,18 +20,24 @@ const video = document.getElementById('background-video');
                     }, delays[index]);
                 });
 
-    
+
+                fetch('https://ipv4.myip.wtf/json')
+                    .then(response => response.json())
+                    .then(data => {
+                        // Fill in the IP info
+                        document.getElementById('ip').textContent = data.YourFuckingIPAddress;
+                        document.getElementById('org').textContent = data.YourFuckingISP;
+                        document.getElementById('city').textContent = data.YourFuckingCity;
+                        document.getElementById('country').textContent = data.YourFuckingCountry;
+                    })
                 fetch('https://ipapi.co/json/')
                     .then(response => response.json())
                     .then(data => {
                         // Fill in the IP info
-                        document.getElementById('ip').textContent = data.ip;
                         document.getElementById('network').textContent = data.network;
-                        document.getElementById('org').textContent = data.org;
                         document.getElementById('version').textContent = data.version;
-                        document.getElementById('city').textContent = data.city;
+                        document.getElementById('org2').textContent = data.org;
                         document.getElementById('region').textContent = data.region;
-                        document.getElementById('country').textContent = data.country_name;
                         document.getElementById('postal').textContent = data.postal;
                         document.getElementById('latitude').textContent = data.latitude;
                         document.getElementById('longitude').textContent = data.longitude;
